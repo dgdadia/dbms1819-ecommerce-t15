1038ebbece18c43542a576b6e46c42754adb6cca
dgdadia <danicadadia.dd@gmail.com>
2018-08-14 13:52:26 +0800

    module3

--- app.js
+++ app.js
@@ -72,11 +72,11 @@ app.get('/orders', function(req, res) {
 });
 
 app.post('/products/:id/forms', function(req, res) {
-	client.query('SELECT products.id AS id FROM products LEFT JOIN brands ON products.brand_id=brands.id RIGHT JOIN products_category ON products.category_id=products_category.id WHERE products.id = '+req.params.id+';')
+	client.query('SELECT products.name AS name FROM products LEFT JOIN brands ON products.brand_id=brands.id RIGHT JOIN products_category ON products.category_id=products_category.id WHERE products.id = '+req.params.id+';')
 	.then((results)=>{
 		console.log ('results?',results);
 		res.render('form',{
-			id: results.rows[0].id
+			name: results.rows[0].name
 		})
 	})
 	.catch((err) => {
